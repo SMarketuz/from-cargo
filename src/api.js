d9f9578fd3a4bd970f171bd0020ff0717770e40a
MOdevco <124789539+SMarketuz@users.noreply.github.com>
2025-07-27 07:31:39 +0500

    Update API endpoint to use production URL

--- src/api.js
+++ src/api.js
@@ -1,2 +1,3 @@
 // export const api = 'https://pres-7xe0.onrender.com/'
-export const api = 'http://localhost:9090/'
+// export const api = 'http://localhost:9090/'
+export const api = 'https://cargo-back-bvvj.onrender.com/'
